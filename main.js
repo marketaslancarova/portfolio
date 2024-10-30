4b3dee1fdbf8b7e5297671496d76ae629b327fbd
Markéta Šlancarová <marketaslancarova@gmail.com>
2024-10-30 14:01:39 +0100

    Main Page with scroll Fullpage.js

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new fullpage("#fullpage", {
+    autoScrolling: true,
+    navigation: false, // Displays navigation dots
+    anchors: ["firstPage", "secondPage", "thirPage"],
+    sectionsColor: ["#1a1a1a", "##1a1a1a"],
+  });
+});
