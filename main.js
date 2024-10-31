420a8a9cf5fee1bbeca184d4fa077324ba64a74a
Markéta Šlancarová <marketaslancarova@gmail.com>
2024-10-31 21:49:34 +0100

    Story section desktop done

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   new fullpage("#fullpage", {
     autoScrolling: true,
     navigation: false, // Displays navigation dots
-    anchors: ["firstPage", "secondPage", "thirPage"],
+    anchors: ["firstPage", "secondPage", "thirPage", "fourthPage"],
     sectionsColor: ["#1a1a1a", "##1a1a1a"],
   });
 });
